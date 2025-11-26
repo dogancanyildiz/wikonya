9d38782b20f66cdc286fd556fb8f5cf73bc9b0fb
Lessyzz <102208615+Lessyzz@users.noreply.github.com>
2025-11-27 01:13:54 +0300

    refactor(academic): remove academic stats component from page

--- frontend/components/academic/academic-page.tsx
+++ frontend/components/academic/academic-page.tsx
@@ -1,7 +1,5 @@
 import { AcademicHeader } from "./academic-header"
 import { FilterChips } from "./filter-chips"
-import { UniversitySelector } from "./university-selector"
-import { AcademicStats } from "./academic-stats"
 import { ResourceGrid } from "./resource-grid"
 
 export function AcademicPage() {
@@ -9,9 +7,9 @@ export function AcademicPage() {
     <div className="max-w-[1440px] mx-auto px-4 sm:px-8 lg:px-16 py-6 sm:py-8">
       <AcademicHeader />
       <FilterChips />
-      <UniversitySelector />
-      <AcademicStats />
-      <ResourceGrid />
+      <div className="mt-2">
+        <ResourceGrid />
+      </div>
     </div>
   )
 }
